dbe71d87e992969dc5efaa74a4891748e9962602
JoaoBacano <91315939+JoaoBacano@users.noreply.github.com>
2023-11-30 13:14:39 +0000

    initial commit

---
+++ app/src/main/java/com/example/kuicly/CursoActivity.java
@@ -0,0 +1,14 @@
+package com.example.kuicly;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CursoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_curso);
+    }
+}
